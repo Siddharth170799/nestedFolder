4e40391ab6ffb985d306338cac061ecacdcd77e0
siddharth <boorgusiddharth@gmail.com>
2025-02-18 14:24:34 +0530

    nestedfolders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+// import json from "./data.json";
+// import { useState } from "react";
 
-function App() {
+
+// const List = ({list,addNodeToList})=>{
+//   const [isExpanded,setIsExpanded] = useState({
+  
+//   })
+
+//   return <div className="container">
+//   {list.map((node) => {
+//     return (
+//       <>
+//       <div key={node.id}>
+//         {node.isFolder && <span onClick={()=>setIsExpanded((prev)=>({...prev,[node.name]: !prev[node.name]}))}>{isExpanded?.[node.name] ? "-" : "+"}</span>}
+
+//         <span>{node.name}</span>
+//        {node.isFolder && <span onClick={()=>addNodeToList(node.id)}> <img src="https://static.vecteezy.com/system/resources/previews/000/439/792/original/vector-folder-icon.jpg" alt="icon" className="icon"/> </span> } 
+//         {isExpanded?.[node.name] && node?.children &&  <List list={node.children} addNodeToList={addNodeToList}/> }
+       
+//         </div>
+   
+//       </>
+//     )
+   
+//   })}
+// </div>
+// }
+// function App() {
+//   const [data, setData] = useState(json);
+//   const addNodeToList=(parentId)=>{
+
+//     const name = prompt("Enter Name")
+
+//     const updateTree=(list)=>{
+// return list.map(node => {
+//   if(node.id == parentId){
+//     return (
+//       {
+//         ...node,
+//         children:[...node.children,{id:"123",name:name,isFolder:"true",children:[]
+
+//         }
+//       ]
+//       }
+//     )
+    
+//   }
+//   if(node.children){
+//     return {...node,children:updateTree(node.children)}
+//   }
+//   return node
+// }
+// )
+//     }
+//     setData(updateTree(data))
+//   }
+//   return (
+//     <div className="App">
+//       <h1>File/Folder Explorer</h1>
+//       <List list={data} addNodeToList={addNodeToList}/>
+     
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react'
+import Practice from './Practice'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Practice/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Practice.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import json from "./data.json";
+import "./App.css";
+
+const List = ({ list, addNodeToList,deleteNodeFromList }) => {
+  const [isExpanded, setIsExpanded] = useState({});
+  return (
+    <div className="container">
+      {list.map((node) => {
+        return (
+          <>
+            <div key={node.id}>
+              {node.isFolder && (
+                <span
+                  onClick={() =>
+                    setIsExpanded({
+                      ...isExpanded,
+                      [node.name]: !isExpanded[node.name],
+                    })
+                  }
+                >
+                  {isExpanded[node.name] ? "-" : "+"}
+                </span>
+              )}
+              <span>{node.name}</span>
+              {node.isFolder && (
+                <>
+                <span onClick={() => addNodeToList(node.id)}>
+                  {" "}
+                  <img
+                    src="https://static.vecteezy.com/system/resources/previews/000/439/792/original/vector-folder-icon.jpg"
+                    alt="icon"
+                    className="icon"
+                  />{" "}
+                </span>
+                <span onClick={()=>deleteNodeFromList(node.id)}>Delete</span>
+                </>
+              )}
+              {isExpanded[node.name] && node?.children && (
+                <List list={node.children} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList} />
+              )}
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+const Practice = () => {
+  const [data, setData] = useState(json);
+
+  const addNodeToList = (parentId) => {
+    const name = prompt("Enter Name");
+    function updateTree(list) {
+      return list.map((node) => {
+        if (node.id == parentId) {
+          return {
+            ...node,
+            children: [
+              ...node.children,
+              { id: "123", name: name, isFolder: "true", children: [] },
+            ],
+          };
+        } else if (node.children) {
+          return { ...node, children: updateTree(node.children) };
+        } else {
+          return node;
+        }
+      });
+    }
+    setData(updateTree(data));
+  };
+  const deleteNodeFromList=(parentId)=>{
+    function updateTree(list){
+        return list.map((node) => {
+            if (node.id == parentId) {
+            return list.filter((node)=> node.id != parentId)
+            } else if (node.children) {
+              return { ...node, children: updateTree(node.children) };
+            } else {
+              return node;
+            }
+          });
+    }
+    setData(updateTree(data))
+  }
+  return (
+    <>
+      <div className="App">
+        <h1>FileExplorer</h1>
+        <List list={data} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList} />
+      </div>
+    </>
+  );
+};
+
+export default Practice;
